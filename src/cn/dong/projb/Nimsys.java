09c7fa010858e8a205d9a2e63425cd4d7726aa8b
dong <dongorigin@gmail.com>
2014-03-26 23:55:23 +0800

    完成主程序命令行交互部分

--- src/cn/dong/projb/Nimsys.java
+++ src/cn/dong/projb/Nimsys.java
@@ -1,5 +1,142 @@
 package cn.dong.projb;
 
+import java.util.Scanner;
+
 public class Nimsys {
-	
+	Scanner mScanner;
+	PlayerCenter mCenter;
+
+	public Nimsys() {
+		mScanner = new Scanner(System.in);
+		mCenter = new PlayerCenter();
+	}
+
+	public void start() {
+		System.out.println("Welcome to Nim\n");
+		System.out.print(">");
+
+		while (true) {
+			parseCommandLine(mScanner.nextLine());
+			System.out.print(">");
+		}
+	}
+
+	private void parseCommandLine(String line) {
+		if (line.length() > 0) {
+			String[] s = line.split(" ", 2);
+			String command = s[0];
+			String action = null;
+			if (s.length > 1) {
+				action = s[1].trim();
+			}
+			parseCommand(command, action);
+		} else {
+			return;
+		}
+	}
+
+	private void parseCommand(String command, String action) {
+		// System.out.println("command:" + command + ", action:" + action);
+		if (command.equals("addplayer")) {
+			addPlayer(action);
+		} else if (command.equals("removeplayer")) {
+			removePlayer(action);
+		} else if (command.equals("editplayer")) {
+			editPlayer(action);
+		} else if (command.equals("resetstats")) {
+			resetStats(action);
+		} else if (command.equals("displayplayer")) {
+			displayPlayer(action);
+		} else if (command.equals("rankings")) {
+			rankings();
+		} else if (command.equals("startgame")) {
+			startGame(action);
+		} else if (command.equals("exit")) {
+			System.exit(0);
+		} else {
+			System.out.println("command not found");
+		}
+		System.out.println();
+	}
+
+	private void addPlayer(String action) {
+		if (action != null) {
+			String[] names = action.split(",");
+			mCenter.addPlayer(names[0], names[1], names[2]);
+		}
+	}
+
+	private void removePlayer(String action) {
+		if (action != null) {
+			// 删除指定Player
+			mCenter.removePlayer(action);
+		} else {
+			// 删除全部Player
+			System.out.println("Are you sure you want to remoe all players? (y/n)");
+			String input = mScanner.next();
+			if (input.equals("y")) {
+				mCenter.removeAllPlayer();
+			}
+		}
+	}
+
+	private void editPlayer(String action) {
+		if (action != null) {
+			String[] names = action.split(",");
+			mCenter.editPlayer(names[0], names[1], names[2]);
+		}
+	}
+
+	private void resetStats(String action) {
+		if (action != null) {
+			// 重置指定Player
+			mCenter.resetStats(action);
+		} else {
+			// 重置全部Player
+			System.out.println("Are you sure you want to reset all player statistics? (y/n)");
+			String input = mScanner.next();
+			if (input.equals("y")) {
+				mCenter.resetAllStats();
+			}
+		}
+	}
+
+	private void displayPlayer(String action) {
+		if (action != null) {
+			// 展示指定Player
+			mCenter.displayPlayer(action);
+		} else {
+			// 展示全部Player
+			mCenter.displayAllPlayer();
+		}
+	}
+
+	private void rankings() {
+		mCenter.rankings();
+	}
+
+	private void startGame(String action) {
+		if (action != null) {
+			// assume valid and correct
+			String[] actions = action.split(",");
+			int stonesNum = Integer.parseInt(actions[0]);
+			int upperBound = Integer.parseInt(actions[1]);
+			String userNameA = actions[2];
+			String userNameB = actions[3];
+
+			Player playerA = mCenter.getPlayer(userNameA);
+			Player playerB = mCenter.getPlayer(userNameB);
+			if (playerA != null && playerB != null) {
+				Nim nim = new Nim(mScanner, stonesNum, upperBound, playerA, playerB);
+				nim.startGame();
+			} else {
+				System.out.println("One of the players does not exist.");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Nimsys nimsys = new Nimsys();
+		nimsys.start();
+	}
 }
--- src/cn/dong/projb/Player.java
+++ src/cn/dong/projb/Player.java
@@ -1,12 +1,13 @@
 package cn.dong.projb;
 
-public class Player {
+public class Player implements Comparable<Player> {
 	private static final String FORMAT_DISPLAY = "%s,%s,%s,%d games,%d wins\n";
 	private String userName;
 	private String givenName;
 	private String familyName;
 	private int playedNum;
 	private int wonNum;
+	private double winRate;
 
 	public Player(String userName, String givenName, String familyName) {
 		setName(userName, givenName, familyName);
@@ -22,12 +23,12 @@ public class Player {
 		return userName;
 	}
 
-	public void play() {
+	public void played(boolean isWin) {
 		playedNum++;
-	}
-
-	public void win() {
-		wonNum++;
+		if (isWin) {
+			wonNum++;
+		}
+		winRate = wonNum / playedNum;
 	}
 
 	public void resetStats() {
@@ -37,6 +38,17 @@ public class Player {
 
 	public void display() {
 		System.out.printf(FORMAT_DISPLAY, userName, givenName, familyName, playedNum, wonNum);
+	}
+
+	@Override
+	public int compareTo(Player player) {
+		if (this.winRate > player.winRate) {
+			return 1;
+		} else if (this.winRate < player.winRate) {
+			return -1;
+		} else {
+			return 0;
+		}
 	}
 
 }
